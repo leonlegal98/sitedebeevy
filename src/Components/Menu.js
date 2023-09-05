701d77055c21a094846619cc76b31904b08237fa
Léon Le Gal <llegal212@gmail.com>
2023-09-05 15:06:37 +0200

    test9

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,32 +1,79 @@
 import '../Styles/Home.css';
 import '../Styles/Menu.css';
-import React from 'react';
+import React, {useState} from "react";
 import {BrowserRouter as Router, Link, Routes, Route} from 'react-router-dom';
 import Bio from './Bio.js';
 import Home from './Home.js';
 
-
 function Menu() {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const menuItems = isMenuOpen ? (
+    <>
+    <div className='responshow'>
+    <img
+        src="menu-icon.png"
+        alt="Menu Icon"
+        className="menu-icon2"
+        onClick={toggleMenu}
+      />
+      <div className="menuresponlien">
+                    <Link to="bio">Bio</Link>
+                    <Link to="atelier">Ateliers d'écriture</Link>
+                    <Link to="tarif">Tarifs</Link>
+                    <Link to="presse">Presse</Link>
+                    <Link to="contact">Contact</Link>
+        </div>
+
+    </div>
+    
+      
+    </>
+  ) : null;
+    
+
     return (
 
         <div>
 
-        <div className="menu">
-            <div className="menu-left">
-            <Link to="/">
-                <img className='logoim1' src="/images/imagegroupbee.png" alt="Image 1"/>
-                </Link>
+            <div className="menu">
+                <div className="menu-left">
+                    <Link to="/">
+                        <img className='logoim1' src="/images/imagegroupbee.png" alt="Image 1"/>
+                    </Link>
+                </div>
+                <div className="menu-right">
+                    <Link to="bio">Bio</Link>
+                    <Link to="atelier">Ateliers d'écriture</Link>
+                    <Link to="tarif">Tarifs</Link>
+                    <Link to="presse">Presse</Link>
+                    <Link to="contact">Contact</Link>
+                </div>
+
             </div>
-            <div className="menu-right">
-                <Link to="bio">Bio</Link>
-                <Link to="atelier">Ateliers d'écriture</Link>
-                <Link to="tarif">Tarifs</Link>
-                <Link to="presse">Presse</Link>
-                <Link to="contact">Contact</Link>
-            </div>
-            
-            
-        </div>
+            <div className='menurespon'>
+
+           
+                        <div className="top-bar"></div>
+                <div className="menu-container">
+                    {menuItems}
+                    <div className='carrerespon'>
+                    <img
+                    src={isMenuOpen ? "/images/logofooterresp.png" : "menu-icon.png"}
+                    alt="Menu Icon"
+                    className="menu-icon"
+                    onClick={toggleMenu}
+                    />
+
+                    </div>
+                    
+      </div>
+      </div>
         </div>
     );
 }
--- src/Components/Podcast.js
+++ src/Components/Podcast.js
@@ -1,100 +1,135 @@
- import Footer from './Footer';
- import React, { useState, useRef } from 'react';
- import '../Styles/Podcast.css';
- 
- function Podcast() {
-   const [currentlyPlaying, setCurrentlyPlaying] = useState(null);
-   const [isPaused, setIsPaused] = useState(true); // Ajoutez un état pour suivre la pause
-   const audioRef = useRef(null);
- 
-   const podcasts = [
-     {
-       title: "Podcast 1",
-       url: "/podcasts/Episode1.mp3",
-       date: "Date du podcast 1",
-     },
-     {
-       title: "Podcast 2",
-       url: "/podcasts/Episode2.mp3",
-       date: "Date du podcast 2",
-     },
-     {
-       title: "Podcast 3",
-       url: "/podcasts/Episode3.mp3",
-       date: "Date du podcast 3",
-     },
-     {
-       title: "Podcast 4",
-       url: "/podcasts/Episode4.mp3",
-       date: "Date du podcast 4",
-     },
-     // Ajoutez d'autres podcasts ici
-   ];
- 
-   const togglePlayPause = (url) => {
-     const audio = audioRef.current;
-     if (currentlyPlaying === url) {
-       if (isPaused) {
-         audio.play();
-         setIsPaused(false);
-       } else {
-         audio.pause();
-         setIsPaused(true);
-       }
-     } else {
-       audio.src = url;
-       audio.play();
-       setCurrentlyPlaying(url);
-       setIsPaused(false);
-     }
-   };
- 
-   const downloadPodcast = (url) => {
-     const link = document.createElement('a');
-     link.href = url;
-     link.download = 'podcast.mp3';
-     link.click();
-   };
- 
-   return (
-     <div>
-       <div className='titrepodcast'>
-         <h1>Podcasts</h1>
-       </div>
- 
-       <div className='podcastbl1'>
-         {/* Contenu de votre premier podcast */}
-       </div>
- 
-       <div className="podcast-page">
-         <h1>Liste de Podcasts</h1>
-         {podcasts.map((podcast) => (
-           <div key={podcast.url} className="podcast-item">
-             <h3>{podcast.title}</h3>
-             <p>Date: {podcast.date}</p>
-             <button
-               className="play-button"
-               onClick={() => togglePlayPause(podcast.url)}
-             >
-               {currentlyPlaying === podcast.url && !isPaused // Utilisez l'état de pause
-                 ? 'Pause'
-                 : 'Écouter'}
-             </button>
-             <button
-               className="download-button"
-               onClick={() => downloadPodcast(podcast.url)}
-             >
-               Télécharger
-             </button>
-           </div>
-         ))}
-       </div>
- 
-       <audio ref={audioRef} />
-       <Footer />
-     </div>
-   );
- }
- 
- export default Podcast;
- 
+import Footer from './Footer';
+import React, {useState, useRef} from 'react';
+import '../Styles/Podcast.css';
+
+function Podcast() {
+    const [currentlyPlaying,
+        setCurrentlyPlaying] = useState(null);
+    const [isPaused,
+        setIsPaused] = useState(true); // Ajoutez un état pour suivre la pause
+    const audioRef = useRef(null);
+
+    const podcasts = [
+        {
+            key: 'episode1',
+            title: 'Episode 1',
+            url: '/podcasts/Episode1.mp3',
+            date: 'Date du Episode 1',
+            texte: 'aa'
+        }, {
+            key: 'episode2',
+            title: 'Episode 2',
+            url: '/podcasts/Episode2.mp3',
+            date: 'Date du Episode 2',
+            texte: 'aa'
+        }, {
+            key: 'episode3',
+            title: 'Episode 3',
+            url: '/podcasts/Episode3.mp3',
+            date: 'Date du Episode 3',
+            texte: 'aa'
+        }, {
+            key: 'episode4',
+            title: 'Episode 4',
+            url: '/podcasts/Episode4.mp3',
+            date: 'Date du Episode 4',
+            texte: 'aa'
+        }, {
+            key: 'episode5',
+            title: 'Episode 5',
+            url: '/podcasts/Episode5.mp3',
+            date: 'Date du Episode 5',
+            texte: 'aa'
+        },
+        // Ajoutez d'autres podcasts ici
+    ];
+
+    const togglePlayPause = (url) => {
+        const audio = audioRef.current;
+        if (currentlyPlaying === url) {
+            if (isPaused) {
+                audio.play();
+                setIsPaused(false);
+            } else {
+                audio.pause();
+                setIsPaused(true);
+            }
+        } else {
+            audio.src = url;
+            audio.play();
+            setCurrentlyPlaying(url);
+            setIsPaused(false);
+        }
+    };
+
+    const downloadPodcast = (url) => {
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'podcast.mp3';
+        link.click();
+    };
+
+    return (
+        <div>
+            <div className='titrepodcast'>
+                <h1>Podcasts</h1>
+            </div>
+
+            <div className='podcastbl1'>
+                <div>
+                    <img src='/images/imagepodcast.png'/>
+                </div>
+
+                <div className='podcastbl1bl2'>
+                    <h3>Dans ma poche,<br></br>
+                        ma mémoire
+                    </h3>
+                    <p>
+                        <span>Novembre 2021 - Mars 2022</span>
+                        <br></br>
+                        -
+                        <br></br>
+                        "Dans ma poche, ma mémoire", un podcast en 5 épisodes réalisé par Charlie
+                        Droesch-Du Cerceau durant les ateliers d'écriture menés à l'EHPAD du Centre
+                        Hospitalier d'Erstein. Projet initié par la Compagnie Actémo Théâtre en
+                        collaboration avec le Service Culturel de la Ville d'Erstein."Dans ma poche, ma
+                        mémoire", un podcast en 5 épisodes réalisé par Charlie Droesch-Du Cerceau durant
+                        les ateliers d'écriture menés à l'EHPAD du Centre Hospitalier d'Erstein. Projet
+                        initié par la Compagnie Actémo Théâtre en collaboration avec le Service Culturel
+                        de la Ville d'Erstein.
+                    </p>
+                </div>
+            </div>
+
+            <div className="podcast-page">
+
+                {podcasts.map((podcast) => (
+                    <div key={podcast.url} className="podcast-item">
+                        <h3>{podcast.title}</h3>
+                        <p>{podcast.texte}</p>
+                        <p>Date: {podcast.date}</p>
+                        <div className='position-buttonpod'>
+                            <button
+                                className="round-button play-button"
+                                onClick={() => togglePlayPause(podcast.url)}>
+                                {currentlyPlaying === podcast.url && !isPaused
+                                    ? (<img src="/images/pause.png" alt="Pause"/>)
+                                    : (<img src="/images/play.png" alt="Écouter"/>)}
+                            </button>
+                            <button
+                                className="round-button2 download-button"
+                                onClick={() => downloadPodcast(podcast.url)}>
+                                <img src="/images/download.png" alt="Télécharger"/>
+                            </button>
+                        </div>
+                    </div>
+                ))}
+            </div>
+
+            <audio ref={audioRef}/>
+            <Footer/>
+        </div>
+    );
+}
+
+export default Podcast;
